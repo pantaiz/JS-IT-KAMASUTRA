6583d35eca3a87faf40357b8190fa8e048306073
Pantaiz <pantaiz@yandex.ru>
2023-01-22 14:18:21 +0300

    25

--- slider/js/script.js
+++ slider/js/script.js
@@ -1,4 +1,4 @@
-var slider1=createNewSlider();
-var slider2=createNewSlider();
+var slider1=sliderFactory.createNewSlider();
+var slider2=sliderFactory.createNewSlider();
 slider1.start('itkamasutra-slider1');
 slider2.start('itkamasutra-slider2');
